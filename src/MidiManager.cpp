3071d19a0112a7767be600da0fec0cb360bcd039
James <jgbentley10@gmail.com>
2016-02-23 17:48:51 +0000

    Added several new effects

---
+++ src/MidiManager.cpp
@@ -0,0 +1,9 @@
+//
+//  midiManager.cpp
+//  
+//
+//  Created by James Bentley on 2/17/16.
+//
+//
+
+#include "midiManager.h"
---
+++ src/MidiManager.h
@@ -0,0 +1,14 @@
+//
+//  midiManager.h
+//  
+//
+//  Created by James Bentley on 2/17/16.
+//
+//
+
+#ifndef ____midiManager__
+#define ____midiManager__
+
+#include <stdio.h>
+
+#endif /* defined(____midiManager__) */
